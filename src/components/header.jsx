94ee513931e5d642eba4cad5a2260d4b40f5af19
danielsantana9824 <108958899+danielsantana9824@users.noreply.github.com>
2025-01-05 15:34:53 -0500

    navBar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,55 @@
-import React from "react";
-import Navigation from "./Navigation";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="header">
-      <h1>Developer Name</h1>
-      <Navigation />
+    <header className="bg-dark text-white py-3">
+      <div className="container d-flex justify-content-between align-items-center">
+        {/* Título del encabezado */}
+        <h1 className="m-0">Daniel Ramirez</h1>
+
+        {/* Barra de navegación */}
+        <nav>
+          <ul className="nav ms-auto">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className="nav-link text-white"
+                activeClassName="active text-warning"
+              >
+                About Me
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/portfolio"
+                className="nav-link text-white"
+                activeClassName="active text-warning"
+              >
+                Portfolio
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                className="nav-link text-white"
+                activeClassName="active text-warning"
+              >
+                Contact
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/resume"
+                className="nav-link text-white"
+                activeClassName="active text-warning"
+              >
+                Resume
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
